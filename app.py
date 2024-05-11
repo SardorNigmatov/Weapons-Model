e56a4ea9826fc6bbb7143605a42002f302895a0e
SardorNigmatov <sardornigmatov2003@gmail.com>
2024-05-11 22:37:55 +0500

    New graph

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 import streamlit as st
 from fastai.vision.all import *
 import pathlib
-import platform
 import plotly.express as px
 import matplotlib.pyplot as plt
-import platform
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Corrected platform check
 if platform.system() == 'Linux':
-    pathlib.WindowsPath = pathlib.PosixPath
+    pathlib.PosixPath = pathlib.WindowsPath
 
 # title
 st.title("Qurollarni klassifikatsiya qiluvchi model")
@@ -31,22 +29,17 @@ if file:
     # Bashorat
     pred, pred_id, probs = model.predict(img)
 
-    # Display prediction
+    # Bashoratni ko'rsatish
     st.success(f"Bashorat qiymat: {pred}")
     st.info(f"Ehtimollik: {probs[pred_id]*100:.1f}%")
 
-    # Plot using Plotly
+    # Chizma sinfi ehtimollik taqsimoti
+    st.subheader("Sinf Ehtimolligi Taqsimoti")
     fig = px.bar(x=probs*100, y=model.dls.vocab)
     st.plotly_chart(fig)
 
-    # Additional Matplotlib plot
-    plt.figure()
-    plt.bar(model.dls.vocab, probs)
-    plt.xlabel('Class')
-    plt.ylabel('Probability')
-    plt.xticks(rotation=90)
-    plt.title('Probability Distribution')
-
-    # Display Matplotlib plot using st.pyplot()
-    st.pyplot()
+    # Confusion Matrix
+    st.subheader("Confusion Matrix")
+    interp = ClassificationInterpretation.from_learner(model)
+    interp.plot_confusion_matrix()
 
